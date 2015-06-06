b4fc58341e17f1bcaad83c98a6728c19acbfb4d9
yasai shota <gityasai@yahoo.co.jp>
2015-06-07 00:16:03 +0900

    Initial Commit

---
+++ secondTests/secondTests.swift
@@ -0,0 +1,36 @@
+//
+//  secondTests.swift
+//  secondTests
+//
+//  Created by gityasai on 2015/06/07.
+//  Copyright (c) 2015年 gityasai. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class secondTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
